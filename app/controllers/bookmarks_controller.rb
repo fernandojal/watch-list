b2e5497ca4db90ff8c2dec5e314d02526120ffc2
Fernando Amundaray <fernandojal.une@gmail.com>
2021-10-24 12:14:43 -0400

    arreglado problema del delete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,12 +1,11 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
 
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
@@ -17,12 +16,17 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
+    redirect_to list_path(@bookmark.list), notice: 'Bookmark was successfully destroyed.'
 
-    redirect_to list_path
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :lists, only: [ :index, :new, :create, :show ] do
     resources :bookmarks, only: [ :new, :create ]
   end
-  resources :bookmarks, only: [:destroy]
+  resources :bookmarks, only: %i[destroy]
+  get '/bookmarks/:id', to: 'bookmarks#destroy'
 end
